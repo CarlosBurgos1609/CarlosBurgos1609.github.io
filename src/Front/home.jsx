e2d794a1e95f764554b2a4317ff8b556a30050d3
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-11-25 23:43:21 -0500

    Update

--- src/Front/home.jsx
+++ src/Front/home.jsx
@@ -4,42 +4,40 @@ import Mylogo from './../img/Mylogo.png'
 
 
 function Home() {
-    return (
-<body>
-    <div class="part">
-    <p>   cab</p>
-    </div>
-    <section class="containers container1">
-  <h1>Logo y Definción</h1>
-  <img src={Mylogo} alt="logo" />
-  <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-    y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-    ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-    explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-    Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-  </p>
-    </section>
-    <section class="containers container1">
-  <h1>Logo y Definción</h1>
-  <img src={Mylogo} alt="logo" />
-  <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-    y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-    ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-    explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-    Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-  </p>
-    </section>
-    <section class="containers container1">
-  <h1>Logo y Definción</h1>
-  <img src={Mylogo} alt="logo" />
-  <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-    y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-    ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-    explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-    Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-  </p>  
-    </section>
-</body >
+  return (
+    <body>
+      <div class="part">
+        <p>   cab</p>
+      </div>
+      <section class="containers container1">
+        <h1>Logo y Definción</h1>
+        <img src={Mylogo} alt="logo" />
+        <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
+          y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
+          ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
+          explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
+          Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
+        </p>
+      </section>
+      <section class="containers container1">
+        <h1>Logo y Definción</h1>
+        <img src={Mylogo} alt="logo" />
+        <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
+          y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
+          ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
+          explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
+          Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
+        </p>
+      </section>
+      <section class="containers container1">
+        <h1>Logo y Definción</h1>
+        <img src={Mylogo} alt="logo" />
+        <iframe src="https://open.spotify.com/embed/playlist/5P6n1BNwogBmLXZqIwlk3e?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/6Zyv4Tzir19kAyHXLYgziF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      </section>
+
+      
+    </body >
 
 
 
