c20ac937df0d26327f14448678935debd2801751
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-11-27 18:39:18 -0500

    Update

--- src/Front/playlists.jsx
+++ src/Front/playlists.jsx
@@ -16,10 +16,16 @@ function PlayLists() {
           <h1>Play Lists</h1>
 
           <div class="play">
-            <div class="colum-1">
+            <div class="colum-2">
               <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
 
+            <div class="colum-2">
+              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+            <div class="colum-2">
+              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
             <div class="colum-2">
               <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
