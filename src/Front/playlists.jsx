bb08566dd63d951e45f01dea968d3db688a4bae2
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-11-27 21:27:35 -0500

    Update

--- src/Front/playlists.jsx
+++ src/Front/playlists.jsx
@@ -8,31 +8,63 @@ function PlayLists() {
   return (
     <body>
       <div class="part">
-        <p>   cab</p>
+        <p>cab</p>
       </div>
 
       <section class="containers">
         <section class="playlist">
           <h1>Play Lists</h1>
+          <div class="wrapper">
+            <div class="slider-container">
+              <div class="slider-item">
+                {/* rock 80 y 90 */}
+                <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+                {/* Salsa */}
+                <iframe src="https://open.spotify.com/embed/playlist/7ARdbqV0W6SQSYyJ0pH3ZY?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Trap argentino */}
+              <iframe  src="https://open.spotify.com/embed/playlist/4lRMQOJAUwlnPN0u6vltCa?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Pop latino */}
+              <iframe src="https://open.spotify.com/embed/playlist/6F2QF1tMHv6U9B8QMn1pbR?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Cumbia */}
+              <iframe src="https://open.spotify.com/embed/playlist/3wdr8eEILMkHJDXNu12Y2N?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Pop en español */}
+              <iframe src="https://open.spotify.com/embed/playlist/40BbhGPEcklGp49QrSlFfZ?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Corridos */}
+              <iframe  src="https://open.spotify.com/embed/playlist/2fYtfgz85qBaUfP10lnZYt?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Merengue */}
+              <iframe  src="https://open.spotify.com/embed/playlist/10nfX6VXflrOquAKmA1bMz?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div class="slider-item">
+              {/* Pop rock */}
+              <iframe src="https://open.spotify.com/embed/playlist/6Zyv4Tzir19kAyHXLYgziF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              <div>
+              {/* Reguetón */}
+              <iframe src="https://open.spotify.com/embed/playlist/6Zyv4Tzir19kAyHXLYgziF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+              
 
-          <div class="play">
-            <div class="colum-2">
-              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-
-            <div class="colum-2">
-              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-            <div class="colum-2">
-              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-            <div class="colum-2">
-              <iframe src="https://open.spotify.com/embed/playlist/6KERsvpYzzhISMRnJz77xh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
           </div>
         </section>
       </section>
-    </body >
+    </body>
+
+
   );
 }
 export default PlayLists;
