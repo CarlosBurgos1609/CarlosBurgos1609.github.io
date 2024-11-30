31eb36cf34b4a9b789a92ba2598eac10a09c1386
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-11-30 14:52:24 -0500

    Update

--- src/Front/languages.jsx
+++ src/Front/languages.jsx
@@ -1,33 +1,40 @@
 import './../Style/style.scss';
-import Mylogo from './../img/Mylogo.png'
-import spotify from './../img/spotify.png'
+import cpp from './../img/cpp.png'
+import css from './../img/css.png'
+
+
+
+
 // import './Style/style.scss';
 
 
 function languages() {
-    return (<div>
-        <div class="part">
-        <p>   cab</p>
+  return (
+    <body>
+      <div class="part">
+        <p>cab</p>
       </div>
-        <body>
-      
-      <section class="containers container1">
-        <h1>Logo y Definción</h1>
-        <div class="log">
-          <div class="logo"><img src={Mylogo} alt="logo" /></div>
-          <div class="text">
-            <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-              y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-              ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-              explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-              Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-            </p>
-          </div>
+      <section class="containers">
+        <section class="languages">
+          <h1>
+            <picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" />
+            </picture> Lenguajes de programación<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture></h1>
+          <div class="wrapper">
+            <div class="slider-container">
+              <div class="slider-item">
+                <img src={cpp} alt="" />
+              </div>
+              <div class="slider-item">
+                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                </a>
+              </div>
 
-        </div>
+            </div>
+          </div>
+        </section>
       </section>
-    </body >
-    </div>
-    );
+    </body>
+  );
 }
 export default languages;
