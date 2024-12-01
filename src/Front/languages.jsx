7a6168e677813dd8f5f8b1caf37f91fa0273f3cd
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-01 14:46:48 -0500

    update

--- src/Front/languages.jsx
+++ src/Front/languages.jsx
@@ -1,10 +1,5 @@
 import './../Style/style.scss';
 import cpp from './../img/cpp.png'
-import css from './../img/css.png'
-
-
-
-
 // import './Style/style.scss';
 
 
@@ -18,13 +13,51 @@ function languages() {
         <section class="languages">
           <h1>
             <picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" />
-            </picture> Lenguajes de programación<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture></h1>
+            </picture> Lenguajes de programación<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture>
+          </h1>
           <div class="wrapper">
             <div class="slider-container">
               <div class="slider-item">
-                <img src={cpp} alt="" />
+                {/* c++  o cpp */}
+                <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a>
               </div>
               <div class="slider-item">
+                {/* css */}
+                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                </a>
+              </div>
+              <div class="slider-item">
+                {/* css */}
+                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                </a>
+              </div>
+              <div class="slider-item">
+                {/* css */}
+                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                </a>
+              </div>
+              <div class="slider-item">
+                {/* css */}
+                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                </a>
+              </div>
+
+            </div>
+          </div>
+        </section>
+        <section class="languages">
+          <h1>
+            <picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" />
+            </picture> Desarrollo de frontent<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture>
+          </h1>
+          <div class="wrapper">
+            <div class="slider-container">
+              <div class="slider-item">
+                {/* css */}
                 <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
                   <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
                 </a>
