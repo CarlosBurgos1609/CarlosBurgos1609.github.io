e4afb3a0ebec7fe88e31c66e4e38aef6199f4511
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-01 17:00:36 -0500

    Update

--- src/Front/languages.jsx
+++ src/Front/languages.jsx
@@ -22,28 +22,25 @@ function languages() {
                 <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a>
               </div>
               <div class="slider-item">
-                {/* css */}
-                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
+                {/* c# */}
+                <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="70" height="70"/> 
                 </a>
               </div>
               <div class="slider-item">
-                {/* css */}
-                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
-                </a>
+                {/* java */}
+                <a href="https://www.java.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="70" height="70"/> </a>
               </div>
               <div class="slider-item">
-                {/* css */}
-                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
-                </a>
+                {/* Java script */}
+                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="70" height="70"/> </a>
               </div>
               <div class="slider-item">
-                {/* css */}
-                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
-                </a>
+                {/* PhP */}
+                <a href="https://www.php.net" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="70" height="70"/> </a>
+              </div>
+              <div class="slider-item">
+                {/* Python */}
+                <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="70" height="70"/> </a>
               </div>
 
             </div>
@@ -52,7 +49,7 @@ function languages() {
         <section class="languages">
           <h1>
             <picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" />
-            </picture> Desarrollo de frontent<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture>
+            </picture> Desarrollo de Frontend y Backend<picture> <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="50px" /></picture>
           </h1>
           <div class="wrapper">
             <div class="slider-container">
@@ -61,6 +58,26 @@ function languages() {
                 <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
                   <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70" />
                 </a>
+              </div>
+              <div class="slider-item">
+                {/* React */}
+                <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="70" height="70"/> </a>
+              </div>
+              <div class="slider-item">
+                {/* html */}
+                <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="70" height="70"/> </a>
+              </div>
+              <div class="slider-item">
+                {/* Sass */}
+                <a href="https://sass-lang.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="70" height="70"/> </a>
+              </div>
+              <div class="slider-item">
+                {/* Node.js */}
+                <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="70" height="70"/> </a>
+              </div>
+              <div class="slider-item">
+                {/* Django */}
+                <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="70" height="70"/> </a>
               </div>
 
             </div>
