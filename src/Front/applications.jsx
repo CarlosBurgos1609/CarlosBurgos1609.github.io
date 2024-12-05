0dfbca166864bdaaea2778d6469eb9ea4306908b
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-04 20:35:39 -0500

    Update Aplications

--- src/Front/applications.jsx
+++ src/Front/applications.jsx
@@ -1,33 +1,29 @@
 import './../Style/style.scss';
 import Mylogo from './../img/Mylogo.png'
 import spotify from './../img/spotify.png'
+import inProcess from './../img/inProcess.png'
+import bici from './../img/bici.png'
 // import './Style/style.scss';
 
 
 function Aplicactions() {
-    return (<div>
-        <div class="part">
-        <p>   cab</p>
-      </div>
-        <body>
-      
-      <section class="containers container1">
-        <h1>Logo y Definción</h1>
-        <div class="log">
-          <div class="logo"><img src={Mylogo} alt="logo" /></div>
-          <div class="text">
-            <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-              y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-              ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-              explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-              Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-            </p>
-          </div>
+  return (<div>
+    <div class="part">
+      <p>   cab</p>
+    </div>
+    <body>
 
-        </div>
+      <section class="containers">
+        <section class="aplications">
+          <h1>Aplicaciones Realizadas</h1>
+          <div class="log">
+            <a href="https://bici-parqueadero.up.railway.app/"><img src={bici} alt="" /></a>
+            <img src={inProcess} alt="InProcess" />
+            </div>
+        </section>
       </section>
     </body >
-    </div>
-    );
+  </div>
+  );
 }
 export default Aplicactions;
