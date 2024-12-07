2ff63e13f0c935f7111a072e482c4b9a6d078455
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-06 22:01:53 -0500

    Update

--- src/Front/socialNetworks.jsx
+++ src/Front/socialNetworks.jsx
@@ -18,18 +18,8 @@ function SocialNetworks() {
               explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
               Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
             </p>
-            <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-              y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-              ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-              explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-              Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-            </p>
-            <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-              y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-              ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-              explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-              Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-            </p>
+            
+            
           </div>
 
         </div>
