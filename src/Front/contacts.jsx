9f4baecbde4ac1b30469622671c0b595a39e500b
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-16 16:00:32 -0500

    Update

--- src/Front/contacts.jsx
+++ src/Front/contacts.jsx
@@ -7,8 +7,14 @@ function Contacts() {
     <body>
       <section class="containers">
         <section class="contact">
+          <div>
+
           <h1>Correo electrónico: caburgos.0325@unicesmag.edu.co</h1>
+          </div>
+          <div>
+
           <h1>Discord</h1>
+          </div>
         </section>
       </section>
       <section class="containers" >
