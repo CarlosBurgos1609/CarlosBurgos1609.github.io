c761fe8e12106ce34ccb7bcaec406bfec8de10d6
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-16 15:34:43 -0500

    Update Certificates

--- src/Front/certificates.jsx
+++ src/Front/certificates.jsx
@@ -13,31 +13,36 @@ function Certificates() {
     <body>
       <section class="containers">
         <section class="certificates">
-        <div class="logo">
-          <a href="https://www.ratatype.es/u5891306/certificate/es/"target='_blank'><img src={ratatype} alt="" /></a>
-          <h1>Ratatype</h1>
-        </div>
-        <div class="certificate">
-          <a href="https://www.ratatype.es/u5891306/certificate/es/" target='_blank'></a><img src={ratatypecr} alt="Ratatype" />
-        </div>
+          <div class="logo">
+            <a href="https://www.ratatype.es/u5891306/certificate/es/" target='_blank'><img src={ratatype} alt="" /></a>
+            <h1>Ratatype</h1>
+          </div>
+          <div class="certificate">
+            <a href="https://www.ratatype.es/u5891306/certificate/es/" target='_blank'></a><img src={ratatypecr} alt="Ratatype" />
+          </div>
         </section>
+      </section>
+
+      <section class="containers">
         <section class="certificates">
-        <div class="logo">
-          <a href="http://senasofiaplus.edu.co/sofia-public/"target='_blank'><img src={sena} alt="" /></a>
-          <h1>SENA</h1>
-        </div>
-        <div class="certificate">
-          <a href="http://senasofiaplus.edu.co/sofia-public/" target='_blank'></a><img src={senacr} alt="Ratatype" />
-        </div>
+          <div class="logo">
+            <a href="http://senasofiaplus.edu.co/sofia-public/" target='_blank'><img src={sena} alt="" /></a>
+            <h1>SENA</h1>
+          </div>
+          <div class="certificate">
+            <a href="http://senasofiaplus.edu.co/sofia-public/" target='_blank'></a><img src={senacr} alt="Ratatype" />
+          </div>
         </section>
+        </section>
+        <section class="containers">
         <section class="certificates">
-        <div class="logo">
-          <a href="#"target='_blank'><img src={scrum} alt="" /></a>
-          <h1>ScrumStudy</h1>
-        </div>
-        <div class="certificate">
-          <a href="https://www.scrumstudy.com/" target='_blank'></a><img src={scrumcr} alt="Ratatype" />
-        </div>
+          <div class="logo">
+            <a href="#" target='_blank'><img src={scrum} alt="" /></a>
+            <h1>ScrumStudy</h1>
+          </div>
+          <div class="certificate">
+            <a href="https://www.scrumstudy.com/" target='_blank'></a><img src={scrumcr} alt="Ratatype" />
+          </div>
         </section>
       </section>
     </body >
--- src/Front/contacts.jsx
+++ src/Front/contacts.jsx
@@ -5,19 +5,18 @@ import Mylogo from './../img/logos/Mylogo.png'
 function Contacts() {
   return (
     <body>
-      <section class="containers container1">
-        <h1>Logo y Definción</h1>
-        <div class="log">
-          <div class="logo"><img src={Mylogo} alt="logo" /></div>
-          <div class="text">
-            <p>Mi logo representa la unión de diferentes facetas de mi personalidad. El círculo azul simboliza mi integridad
-              y la búsqueda de un equilibrio en mi vida. Los triángulos, con su forma sólida y ascendente, representan mis
-              ambiciones y mi constante crecimiento. El semicírculo, que se abre hacia el futuro, simboliza mi deseo de
-              explorar nuevas oportunidades. La combinación de estas formas refleja mi nombre, Carlos Alexander Burgos
-              Jaramillo, donde cada elemento visual representa una parte esencial de quién soy.
-            </p>
-          </div>
-        </div>
+      <section class="containers">
+        <section class="contact">
+          <h1>Correo electrónico: caburgos.0325@unicesmag.edu.co</h1>
+          <h1>Discord</h1>
+        </section>
+      </section>
+      <section class="containers" >
+
+      <section class="social">
+        <p>Carlos</p>
+        <img src={Mylogo} alt="logo" />
+      </section>
       </section>
     </body >
   );
