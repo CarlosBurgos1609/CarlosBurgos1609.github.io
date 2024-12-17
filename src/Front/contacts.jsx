f188bd3079c16be1d596cc9e6fb944d7d9c3cb32
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2024-12-16 22:23:04 -0500

    Update Contacts

--- src/Front/contacts.jsx
+++ src/Front/contacts.jsx
@@ -23,16 +23,16 @@ function Contacts() {
 
           <p align="center">
 
-            <a href="https://www.linkedin.com/in/carlos-alexander-burgos-jaramillo-3171b533a/"><img src="https://img.shields.io/badge/linkedin-%230A66C2.svg?style=plastic&logo=linkedin&logoColor=white" alt="LinkedIn" /></a>
-            <a href="https://www.facebook.com/profile.php?id=61569174632410"><img src="https://img.shields.io/badge/facebook-%231877F2.svg?style=plastic&logo=facebook&logoColor=white" alt="Facebook" /></a>
-            <a href="https://www.instagram.com/carlitos_burgosj/"><img src="https://img.shields.io/badge/instagram-%23E4405F.svg?style=plastic&logo=instagram&logoColor=white" alt="Instagram" /></a>
-            <a href="https://github.com/CarlosBurgos1609"><img src="https://img.shields.io/badge/github-%23181717.svg?style=plastic&logo=github&logoColor=white" alt="GitHub" /></a>
-            <a href="#"><img src="https://img.shields.io/badge/gmail-%23EA4335.svg?style=plastic&logo=gmail&logoColor=white" alt="Gmail" /></a>
-            <a href="#"><img src="https://img.shields.io/badge/whatsapp-%2325D366.svg?style=plastic&logo=whatsapp&logoColor=white" alt="Whatsapp" /></a>
+            <a href="https://www.linkedin.com/in/carlos-alexander-burgos-jaramillo-3171b533a/"target='_blank'><img src="https://img.shields.io/badge/linkedin-%230A66C2.svg?style=plastic&logo=linkedin&logoColor=white" alt="LinkedIn" /></a>
+            <a href="https://www.facebook.com/profile.php?id=61569174632410"target='_blank'><img src="https://img.shields.io/badge/facebook-%231877F2.svg?style=plastic&logo=facebook&logoColor=white" alt="Facebook" /></a>
+            <a href="https://www.instagram.com/carlitos_burgosj/"target='_blank'><img src="https://img.shields.io/badge/instagram-%23E4405F.svg?style=plastic&logo=instagram&logoColor=white" alt="Instagram" /></a>
+            <a href="https://github.com/CarlosBurgos1609" target='_blank'><img src="https://img.shields.io/badge/github-%23181717.svg?style=plastic&logo=github&logoColor=white" alt="GitHub" /></a>
+            <a href="#"target='_blank'><img src="https://img.shields.io/badge/gmail-%23EA4335.svg?style=plastic&logo=gmail&logoColor=white" alt="Gmail" /></a>
+            <a href="#"target='_blank'><img src="https://img.shields.io/badge/whatsapp-%2325D366.svg?style=plastic&logo=whatsapp&logoColor=white" alt="Whatsapp" /></a>
             <a href="https://discord.gg/yZzMFqZp"><img src="https://img.shields.io/badge/Discord-%230A66C2.svg?style=plastic&logo=discord&logoColor=white" alt="Discord" /></a>
-            <a href="https://open.spotify.com/user/yw3aqvvd0zb5rsjn8wz0kez0j"><img src="https://img.shields.io/badge/Spotify-%2325D366.svg?style=plastic&logo=spotify&logoColor=white" alt="Whatsapp" /></a>
+            <a href="https://open.spotify.com/user/yw3aqvvd0zb5rsjn8wz0kez0j" target='_blank'><img src="https://img.shields.io/badge/Spotify-%2325D366.svg?style=plastic&logo=spotify&logoColor=white" alt="Whatsapp" /></a>
             <a href="https://www.tiktok.com/@carlitosburgosj1609" target='_blank'><img src="https://img.shields.io/badge/Tiktok-%23181717.svg?style=plastic&logo=tiktok&logoColor=white" alt="Tiktok" /></a>
-            <a href="#"><img src="https://img.shields.io/badge/snapchat-%23FFFC00.svg?style=plastic&logo=snapchat&logoColor=white" alt="Snap Chat" /></a>
+            <a href="#"target='_blank'><img src="https://img.shields.io/badge/snapchat-%23FFFC00.svg?style=plastic&logo=snapchat&logoColor=white" alt="Snap Chat" /></a>
           </p>
 
 
