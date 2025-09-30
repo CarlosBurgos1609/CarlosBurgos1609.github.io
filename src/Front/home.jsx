14a760ad698f7f683dd13222c836cb866cd966b9
Carlos Burgos <carlitosaburgosj1609@gmail.com>
2025-09-30 11:09:12 -0500

    Update home and styles

--- src/Front/home.jsx
+++ src/Front/home.jsx
@@ -139,110 +139,161 @@ function Home() {
                 </div>
               </div>
             </div>
-          </div>
-        </section>
 
-        {/* Technologies Section */}
-        <section className="technologies-section">
-          <h2>
-            <picture> 
-              <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="30px" />
-            </picture>
-            Languages and Tools
-            <picture> 
-              <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="30px" />
-            </picture>
-          </h2>
-          <div className="tech-grid">
-            {/* Web Development */}
-            <div className="tech-category">
-              <h3>Web Development</h3>
-              <div className="tech-icons">
-                <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" />
-                </a>
-                <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" />
-                </a>
-                <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" />
-                </a>
-                <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" />
-                </a>
-                <a href="https://sass-lang.com" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40" />
-                </a>
+            {/* Technologies Section */}
+            <div className="technologies-section">
+              <h2>
+                <picture> 
+                  <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="30px" />
+                </picture>
+                Tech Stack & Skills
+                <picture> 
+                  <img src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Programming_Languages.gif?raw=true" width="30px" />
+                </picture>
+              </h2>
+              <div className="tech-grid">
+                {/* Programming Languages */}
+                <div className="tech-category">
+                  <h3>Programming Languages</h3>
+                  <div className="tech-icons">
+                    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" />
+                    </a>
+                    <a href="https://www.python.org" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40" />
+                    </a>
+                    <a href="https://www.java.com" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40" />
+                    </a>
+                    <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="40" height="40" />
+                    </a>
+                    <a href="https://dart.dev" target="_blank" rel="noreferrer">
+                      <img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" alt="dart" width="40" height="40" />
+                    </a>
+                    <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40" />
+                    </a>
+                    <a href="https://www.php.net" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* Frontend Development */}
+                <div className="tech-category">
+                  <h3>Frontend Development</h3>
+                  <div className="tech-icons">
+                    <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" />
+                    </a>
+                    <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" />
+                    </a>
+                    <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" />
+                    </a>
+                    <a href="https://sass-lang.com" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* Backend Development */}
+                <div className="tech-category">
+                  <h3>Backend Development</h3>
+                  <div className="tech-icons">
+                    <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" />
+                    </a>
+                    <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer">
+                      <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* Mobile Development */}
+                <div className="tech-category">
+                  <h3>Mobile Development</h3>
+                  <div className="tech-icons">
+                    <a href="https://flutter.dev" target="_blank" rel="noreferrer">
+                      <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" width="40" height="40" />
+                    </a>
+                    <a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
+                      <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40" />
+                    </a>
+                    <a href="https://developer.android.com" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* Databases */}
+                <div className="tech-category">
+                  <h3>Databases</h3>
+                  <div className="tech-icons">
+                    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40" />
+                    </a>
+                    <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40" />
+                    </a>
+                    <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
+
+                {/* Tools & Technologies */}
+                <div className="tech-category">
+                  <h3>Tools & Technologies</h3>
+                  <div className="tech-icons">
+                    <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+                      <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" />
+                    </a>
+                    <a href="https://postman.com" target="_blank" rel="noreferrer">
+                      <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40" />
+                    </a>
+                    <a href="https://www.linux.org/" target="_blank" rel="noreferrer">
+                      <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40" />
+                    </a>
+                    <a href="https://unity.com/" target="_blank" rel="noreferrer">
+                      <img src="https://www.vectorlogo.zone/logos/unity3d/unity3d-icon.svg" alt="unity" width="40" height="40" />
+                    </a>
+                    <a href="https://www.mathworks.com/" target="_blank" rel="noreferrer">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png" alt="matlab" width="40" height="40" />
+                    </a>
+                    <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer">
+                      <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width="40" height="40" />
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
 
-            {/* Mobile Development */}
-            <div className="tech-category">
-              <h3>Mobile Development</h3>
-              <div className="tech-icons">
-                <a href="https://flutter.dev" target="_blank" rel="noreferrer">
-                  <img src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg" alt="flutter" width="40" height="40" />
-                </a>
-                <a href="https://dart.dev" target="_blank" rel="noreferrer">
-                  <img src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg" alt="dart" width="40" height="40" />
-                </a>
-                <a href="https://reactnative.dev/" target="_blank" rel="noreferrer">
-                  <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40" />
-                </a>
-                <a href="https://developer.android.com" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original-wordmark.svg" alt="android" width="40" height="40" />
-                </a>
+            {/* Spotify Section */}
+            <div className="spotify-section">
+              <h2>
+                <img src={spotify} alt="spotify" width="30" /> 
+                My Music 
+                <img src={spotify} alt="spotify" width="30" />
+              </h2>
+              <div className="spotify-content">
+                <div className="spotify-now-playing">
+                  <a href="https://open.spotify.com/user/yw3aqvvd0zb5rsjn8wz0kez0j" target="_blank" rel="noreferrer">
+                    <img src="https://novatorem-sage-one.vercel.app/api/spotify?background_color=0d1117&border_color=ffffff" alt="Spotify Now Playing" />
+                  </a>
+                </div>
+                <div className="spotify-playlists-preview">
+                  <iframe src="https://open.spotify.com/embed/playlist/5P6n1BNwogBmLXZqIwlk3e?utm_source=generator" width="100%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                  <iframe src="https://open.spotify.com/embed/playlist/7ARdbqV0W6SQSYyJ0pH3ZY?utm_source=generator" width="100%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
+                <div className="spotify-button">
+                  <a href="https://carlosburgos1609.github.io/build/#/playlists">
+                    <button className="view-all-playlists">View all playlists →</button>
+                  </a>
+                </div>
               </div>
-            </div>
-
-            {/* Backend & Databases */}
-            <div className="tech-category">
-              <h3>Backend & Databases</h3>
-              <div className="tech-icons">
-                <a href="https://www.w3schools.com/cs/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg" alt="csharp" width="40" height="40" />
-                </a>
-                <a href="https://www.java.com" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" alt="java" width="40" height="40" />
-                </a>
-                <a href="https://www.python.org" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40" />
-                </a>
-                <a href="https://www.mysql.com/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40" />
-                </a>
-                <a href="https://www.postgresql.org" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40" />
-                </a>
-                <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
-                  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/* Spotify Section */}
-        <section className="spotify-section">
-          <h2>
-            <img src={spotify} alt="spotify" width="30" /> 
-            My Music 
-            <img src={spotify} alt="spotify" width="30" />
-          </h2>
-          <div className="spotify-content">
-            <div className="spotify-now-playing">
-              <a href="https://open.spotify.com/user/yw3aqvvd0zb5rsjn8wz0kez0j" target="_blank" rel="noreferrer">
-                <img src="https://novatorem-sage-one.vercel.app/api/spotify?background_color=0d1117&border_color=ffffff" alt="Spotify Now Playing" />
-              </a>
-            </div>
-            <div className="spotify-playlists-preview">
-              <iframe src="https://open.spotify.com/embed/playlist/5P6n1BNwogBmLXZqIwlk3e?utm_source=generator" width="100%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-              <iframe src="https://open.spotify.com/embed/playlist/7ARdbqV0W6SQSYyJ0pH3ZY?utm_source=generator" width="100%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-            </div>
-            <div className="spotify-button">
-              <a href="https://carlosburgos1609.github.io/build/#/playlists">
-                <button className="view-all-playlists">View all playlists →</button>
-              </a>
             </div>
           </div>
         </section>
